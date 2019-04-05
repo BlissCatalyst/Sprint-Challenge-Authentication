44011632cf612f5be97fe62822d6125169e127cd
Logan Hufstetler <logan.hufstetler@gmail.com>
2019-04-05 10:08:14 -0600

    Added register and saved the password as a hash using bcrypt.

--- config/routes.js
+++ config/routes.js
@@ -1,32 +1,61 @@
-const axios = require('axios');
+const axios = require("axios");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const db = require("../database/dbConfig.js");
 
-const { authenticate } = require('../auth/authenticate');
+const { authenticate } = require("../auth/authenticate");
 
 module.exports = server => {
-  server.post('/api/register', register);
-  server.post('/api/login', login);
-  server.get('/api/jokes', authenticate, getJokes);
+  server.post("/api/register", register);
+  server.post("/api/login", login);
+  server.get("/api/jokes", authenticate, getJokes);
 };
 
-function register(req, res) {
-  // implement user registration
+async function register(req, res) {
+  try {
+    let newCreds = req.body;
+    if (newCreds && newCreds.username && newCreds.password) {
+      newCreds.password = bcrypt.hashSync(newCreds.password, 10);
+      const [id] = await db("users").insert(newCreds);
+
+      const newUser = await db("users")
+        .select("id", "username")
+        .where({ id })
+        .first();
+
+      res
+        .status(201)
+        .json({ newUser, message: "welcome! Thank you for registering!" });
+    } else {
+      res
+        .status(400)
+        .json({ message: "You must include your creds, no free rides" });
+    }
+  } catch (err) {
+    res
+      .status(500)
+      .json({
+        message: "Server wont do that right now...It hates you.",
+        error: err
+      });
+  }
 }
 
-function login(req, res) {
+async function login(req, res) {
   // implement user login
 }
 
 function getJokes(req, res) {
   const requestOptions = {
-    headers: { accept: 'application/json' },
+    headers: { accept: "application/json" }
   };
 
   axios
-    .get('https://icanhazdadjoke.com/search', requestOptions)
+    .get("https://icanhazdadjoke.com/search", requestOptions)
     .then(response => {
       res.status(200).json(response.data.results);
     })
     .catch(err => {
-      res.status(500).json({ message: 'Error Fetching Jokes', error: err });
+      res.status(500).json({ message: "Error Fetching Jokes", error: err });
     });
 }
